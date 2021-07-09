2be80a8ed2d0b2674e382ad9d5bd872cd7f66762
Revathi Chevuri <revathichevuri14@gmail.com>
2021-07-10 00:25:28 +0530

    FormArray concept commit

--- src/app/employee/reactive form/reactive-employee.component.ts
+++ src/app/employee/reactive form/reactive-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { EmployeeService } from '../employee.service';
 
@@ -17,31 +17,37 @@ export class ReactiveEmployeeComponent implements OnInit {
 
   ngOnInit(): void {
     this.employeeForm = this.fb.group({ // building the form using formBuilder
-      id: ['',Validators.required],    //in the formBuilder - we are creating a group of formelements(these should be exactly sameasyour form)
-      firstName: ['',Validators.required],
-      lastName: ['',Validators.required],
-      email: ['',[Validators.email,Validators.required]],
-      mobileNumber: ['',[Validators.minLength(10), Validators.required]],
-      skills: this.fb.group({
-        skillName: [''],
-        experienceInYears: [''],
-        proficiency: ['']
-      })
+      id: ['', Validators.required],    //in the formBuilder - we are creating a group of formelements(these should be exactly sameasyour form)
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.email, Validators.required]],
+      mobileNumber: ['', [Validators.minLength(10), Validators.required]],
+      skills: this.fb.array([
+        this.addSkillFormGroup()
+      ])
     });
   }
 
   onSubmit(): void {
     const formData = this.employeeForm.value;
-    console.log(formData);
-    const newFormdata = { id: formData.id, firstName: formData.firstName, lastName: formData.lastName, email: formData.email, mobileNumber: formData.mobileNumber };
+    const newFormdata = { id: formData.id, firstName: formData.firstName, lastName: formData.lastName, email: formData.email, mobileNumber: formData.mobileNumber, skills: formData.skills };
     this.empService.addEmployee(newFormdata).subscribe(
       response => { this.router.navigate(['employee/list']) }
     );
   }
 
+  get skillControls() {
+    return (<FormArray>this.employeeForm.get('skills')).controls;
+  }
   addSkillButtonClick(): void {
-    console.log("click event completed successfully");
-    console.log(this.employeeForm.value.skills);
+    (<FormArray>this.employeeForm.get('skills')).push(this.addSkillFormGroup());
   }
 
+  addSkillFormGroup(): FormGroup {
+    return this.fb.group({
+      skillName: [''],
+      experienceInYears: [''],
+      proficiency: ['']
+    });
+  }
 }
